1e015f71fe3ebd1ae0841f1110e751f1443d8f5a
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2020-06-06 14:20:41 -0300

    Criando tela inicial e arquivo de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-
-import Header from './Header';
+//Paginas
+import Routes from './routes';
 
 function App() {
   
-  const [counter, setCounter] = useState(0);  //[Valor do estado, Função para atualizar o valor do estado]
-  
-  function handleButtonClick() {
-    setCounter(counter + 1);
-    console.log(counter)
-  }
-
   return (
     <div>
-      <Header title={`Contador: ${counter}`}/>
-      <h1>Conteudo da aplicação</h1>
-      <h2>Valor do contador: {counter}</h2>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <Routes />
     </div>
   );
 }
