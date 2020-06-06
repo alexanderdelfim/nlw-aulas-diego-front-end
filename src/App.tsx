4b89fd40c96d2a6f0e10dbe03e4e6c2283ff1b2a
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2020-06-06 12:19:11 -0300

    Entendendo conceitos do React.JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
+import Header from './Header';
+
 function App() {
+  
+  const [counter, setCounter] = useState(0);  //[Valor do estado, Função para atualizar o valor do estado]
+  
+  function handleButtonClick() {
+    setCounter(counter + 1);
+    console.log(counter)
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title={`Contador: ${counter}`}/>
+      <h1>Conteudo da aplicação</h1>
+      <h2>Valor do contador: {counter}</h2>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
